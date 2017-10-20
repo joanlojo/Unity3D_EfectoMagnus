45bc454e13ce6468acef6232d7e15d97b00570ed
StormValen <valentin.g.l@gmail.com>
2017-10-20 18:00:34 +0200

    vector 3 class

---
+++ PracticaMecanicaFutbol/Assets/Scripts/Vector3.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3 : MonoBehaviour {
+
+	private int X, Y, Z;
+	public void CrossProduct(){
+		
+	}
+	public void DotProduct(){
+
+	}
+	public void Normalize(){
+
+	}
+	public void Module(){
+
+	}
+	public void Plus(){
+
+	}
+	public void Substract(){
+
+	}
+	public void Divide(){
+
+	}
+}
