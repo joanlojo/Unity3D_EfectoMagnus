431c9834c14185107217d5dac852a28364798cb8
StormValen <valentin.g.l@gmail.com>
2017-11-22 18:05:50 +0100

    La pelota rota

--- PracticaMecanicaFutbol/Assets/Scripts/BallPhysics.cs
+++ PracticaMecanicaFutbol/Assets/Scripts/BallPhysics.cs
@@ -28,6 +28,8 @@ public class BallPhysics : MonoBehaviour
     float Cm; //Coeficiente magnus.
     float inertiaMoment;
 
+    bool startKicked = false;
+
     public Our_Vector3 getKickPosition;
     Our_Vector3 puntoDeImpacto = new Our_Vector3(0, 0, 0);
 
@@ -57,31 +59,37 @@ public class BallPhysics : MonoBehaviour
         airDensity = 1.23f;
         Cd = 0.25f;
         Cm = 0.25f;
-        mass = 0.396f;
-        radius = 0.279f;
+        mass = 0.396f; //0.396f
+        radius = 0.279f; //0.279f
         dt = 0.01f;
         fGravity = new Our_Vector3(0, 0, -mass * gravity);
         fMagnus = new Our_Vector3(0, 0, 0);
         fDrag = new Our_Vector3(0, 0, 0);
         fTau = new Our_Vector3(0, 0, 0);
 
+        barra.value *= 50;
        // puntoDeImpacto.x = VectorDireccion.transform.position.x - transform.position.x;//
        // puntoDeImpacto.y = VectorDireccion.transform.position.y - transform.position.y;//PUNTO DONDE SE APLICA FP, ESTA RESPECTO AL CENTRO DE LA PELOTA
        // puntoDeImpacto.z = VectorDireccion.transform.position.z - transform.position.z;//
 
-        Debug.Log(getKickPosition.x);
+        rad.x = getKickPosition.x - transform.position.x;// 
+        rad.y = getKickPosition.y - transform.position.y;// VECTOR ENTRE EL CENTRO DE LA PELOTA Y EL PUNTO DE IMPACTO
+        rad.z = getKickPosition.z - transform.position.z;//
 
-        rad.x = getKickPosition.x - position.x;// 
-        rad.y = getKickPosition.y - position.y;// VECTOR ENTRE EL CENTRO DE LA PELOTA Y EL PUNTO DE IMPACTO
-        rad.z = getKickPosition.z - position.z;//
+        //fP.module = barra.value;// FUERZA TOTAL DE FP
+       
+        dirfP.x = (getKickPosition.x - VectorDireccion.position.x);//
+        dirfP.y = (getKickPosition.y - VectorDireccion.position.y);//ESTO TE DA LA DIRECCION DEL VECTOR, PERO NO FP
+        dirfP.z = (getKickPosition.z - VectorDireccion.position.z);//
 
-        fP.module = barra.value;// FUERZA TOTAL DE FP
-        dirfP.x = getKickPosition.x - VectorDireccion.rotation.x;//
-        dirfP.y = getKickPosition.y - VectorDireccion.rotation.y;//ESTO TE DA LA DIRECCION DEL VECTOR, PERO NO FP
-        dirfP.z = getKickPosition.z - VectorDireccion.rotation.z;//
-        //HAY Q CALCULAR LAS COMPONENTES DE FP
+        //FP EN SUS COMPONENTES 
+        dirfP.Normalize();
+        fP.x = dirfP.x * barra.value;
+        fP.y = dirfP.y * barra.value;
+        fP.z = dirfP.z * barra.value;
+       
 
-        inertiaMoment = (2 / 3) * mass * (radius * radius);
+       inertiaMoment = (0.667f) * mass * (radius * radius);
         Kd = (1 / 2) * airDensity * Cd * area;
         Km = (1 / 2) * airDensity * Cm * area;
 
@@ -96,6 +104,15 @@ public class BallPhysics : MonoBehaviour
         lVelocityInit.y = (fP.y * dt) / mass;
         lVelocityInit.z = (fP.z * dt) / mass;
         //POR UN LADO HAY Q DARLE UNA VELOCIDAD ANGULAR A LA PELOTA --> WVELOCITY, ES SIEMPRE LA MISMA CREO*
+
+        //transform.rotation.Set(fTau.x*wVelocity.x, fTau.y * wVelocity.y, fTau.z * wVelocity.z, 1);
+
+
+        Debug.Log(wVelocity.Module());
+        
+
+
+        transform.rotation = new Quaternion(fTau.x, fTau.y, fTau.z, 0);
         //POR OTRO LADO HAY Q DARLE EL EJE DE ROTACION --> FTAU Q ES PARA TODA LA EJECUCION EL MISMO
     }
 
@@ -104,30 +121,36 @@ public class BallPhysics : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
            startKick();
+            startKicked = true;
         }
-    /*    //EL PRIMER FRAME UTILIZA LA VELOCIDAD INICIAL, A PARTIR DE AHI SE DEBE ACTUALIZAR
-        //Calcular fDrag
-        fDrag.x = -Kd * lVelocityInit.Module() * lVelocityFin.x;
-        fDrag.y = -Kd * lVelocityInit.Module() * lVelocityFin.y;
-        fDrag.z = -Kd * lVelocityInit.Module() * lVelocityFin.z;
 
-        //Calcular fMagnus
-        wVelocity.Normalize();
-        fMagnus.x = Km * lVelocityInit.Module() * (wVelocity.y * lVelocityInit.z - lVelocityInit.y * wVelocity.z);
-        fMagnus.y = Km * lVelocityInit.Module() * (wVelocity.x * lVelocityInit.z - lVelocityInit.x * wVelocity.z);
-        fMagnus.z = Km * lVelocityInit.Module() * (wVelocity.x * lVelocityInit.y - lVelocityInit.x * wVelocity.y);
+        if (startKicked == true){
+         transform.Rotate(new Vector3(fTau.x, fTau.y,fTau.z),20.0f);
+            Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(dirfP.x,dirfP.y,dirfP.z),Color.black);
+        }
+        /*    //EL PRIMER FRAME UTILIZA LA VELOCIDAD INICIAL, A PARTIR DE AHI SE DEBE ACTUALIZAR
+            //Calcular fDrag
+            fDrag.x = -Kd * lVelocityInit.Module() * lVelocityFin.x;
+            fDrag.y = -Kd * lVelocityInit.Module() * lVelocityFin.y;
+            fDrag.z = -Kd * lVelocityInit.Module() * lVelocityFin.z;
 
-        //Agrupar fTotal
-        fTotal.x = fDrag.x + fMagnus.x;//
-        fTotal.y = fDrag.y + fMagnus.y;//CREO QUE NO HACE FALTA
-        fTotal.z = fDrag.z + fMagnus.z + fGravity.z;//
+            //Calcular fMagnus
+            wVelocity.Normalize();
+            fMagnus.x = Km * lVelocityInit.Module() * (wVelocity.y * lVelocityInit.z - lVelocityInit.y * wVelocity.z);
+            fMagnus.y = Km * lVelocityInit.Module() * (wVelocity.x * lVelocityInit.z - lVelocityInit.x * wVelocity.z);
+            fMagnus.z = Km * lVelocityInit.Module() * (wVelocity.x * lVelocityInit.y - lVelocityInit.x * wVelocity.y);
 
-        //ACTUALIZAR lVelocityInit A PARTIR DE LAS NUEVAS FORUMLAS CON EL METODO DE EULER
-        //vanterior = lVelocityFin
-        //act v anterior
-        //modificar lVelocityFin = vanterior
+            //Agrupar fTotal
+            fTotal.x = fDrag.x + fMagnus.x;//
+            fTotal.y = fDrag.y + fMagnus.y;//CREO QUE NO HACE FALTA
+            fTotal.z = fDrag.z + fMagnus.z + fGravity.z;//
 
-        //CALCULAR DERIVADA DE POSICION
-        //MODIFICAR LA POSITION DE LA PELOTA*/
+            //ACTUALIZAR lVelocityInit A PARTIR DE LAS NUEVAS FORUMLAS CON EL METODO DE EULER
+            //vanterior = lVelocityFin
+            //act v anterior
+            //modificar lVelocityFin = vanterior
+
+            //CALCULAR DERIVADA DE POSICION
+            //MODIFICAR LA POSITION DE LA PELOTA*/
     }
 }
