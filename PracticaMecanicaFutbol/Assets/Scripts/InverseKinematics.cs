ffc24f45bc3d80e3c85f1d1f6b2d024af276c8bf
joanlojo <joanlojo.7@gmail.com>
2017-11-26 14:25:35 +0100

    robot

---
+++ PracticaMecanicaFutbol/Assets/Scripts/InverseKinematics.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ENTICourse.IK
+{
+
+    // A typical error function to minimise
+    public delegate float ErrorFunction(Vector3 target, float[] solution);
+
+    public struct PositionRotation
+    {
+        Vector3 position;
+        Quaternion rotation;
+
+        public PositionRotation(Vector3 position, Quaternion rotation)
+        {
+            this.position = position;
+            this.rotation = rotation;
+        }
+
+        // PositionRotation to Vector3
+        public static implicit operator Vector3(PositionRotation pr)
+        {
+            return pr.position;
+        }
+        // PositionRotation to Quaternion
+        public static implicit operator Quaternion(PositionRotation pr)
+        {
+            return pr.rotation;
+        }
+    }
+
+    //[ExecuteInEditMode]
+    public class InverseKinematics : MonoBehaviour
+    {
+        [Header("Joints")]
+        public Transform BaseJoint;
+
+
+      //  [ReadOnly]
+        public RobotJoints[] Joints = null;
+        // The current angles
+        //[ReadOnly]
+        public float[] Solution = null;
+
+        [Header("Destination")]
+        public Transform Effector;
+        [Space]
+        public Transform Destination;
+        public float DistanceFromDestination;
+        private Vector3 target;
+
+        [Header("Inverse Kinematics")]
+        [Range(0, 1f)]
+        public float DeltaGradient = 0.1f; // Used to simulate gradient (degrees)
+        [Range(0, 100f)]
+        public float LearningRate = 0.1f; // How much we move depending on the gradient
+
+        [Space()]
+        [Range(0, 0.25f)]
+        public float StopThreshold = 0f; // If closer than this, it stops
+        [Range(0, 10f)]
+        public float SlowdownThreshold = 0f; // If closer than this, it linearly slows down
+
+
+        public ErrorFunction ErrorFunction;
+
+
+
+        [Header("Debug")]
+        public bool DebugDraw = true;
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+            if (Joints == null)
+                GetJoints();
+
+            ErrorFunction = DistanceFromTarget;
+        }
+
+       // [ExposeInEditor(RuntimeOnly = false)]
+        public void GetJoints()
+        {
+            Joints = BaseJoint.GetComponentsInChildren<RobotJoints>();
+            Solution = new float[Joints.Length];
+        }
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Do we have to approach the target?
+            //TODO
+            target = Destination.transform.position;
+            // ApproachTarget(target);
+            //ForwardKinematics(Solution);
+            if (DistanceFromTarget(target, Solution) > StopThreshold)
+            {
+                ApproachTarget(target);
+                for (int i = 0; i < Joints.Length; i++)
+                {
+                    Joints[i].MoveArm(Solution[i]);
+                }
+            }
+
+            if (DebugDraw)
+            {
+                Debug.DrawLine(Effector.transform.position, target, Color.green);
+                Debug.DrawLine(Destination.transform.position, target, new Color(0, 0.5f, 0));
+            }
+        }
+
+        public void ApproachTarget(Vector3 target)
+        {
+            //TODO
+            for (int i = 0; i < Solution.Length; i++)
+            {
+                float gradient = CalculateGradient(target, Solution, i, DeltaGradient);
+                Solution[i] -= LearningRate * gradient;
+            }
+        }
+
+
+        public float CalculateGradient(Vector3 target, float[] Solution, int i, float delta)
+        {
+            //TODO 
+            float gradient = 0;
+            float solutionAngle = Solution[i];
+            float distance = DistanceFromTarget(target, Solution);
+            Solution[i] += delta;
+            float distancePlus = DistanceFromTarget(target, Solution);
+            gradient = (distancePlus - distance) / delta;
+
+            return gradient; // esto es sin multiplicar, pero creo q solo hace q vaya mas rapido
+        }
+
+        // Returns the distance from the target, given a solution
+        public float DistanceFromTarget(Vector3 target, float[] Solution)
+        {
+            Vector3 point = ForwardKinematics(Solution);
+            return Vector3.Distance(point, target);
+        }
+
+
+        /* Simulates the forward kinematics,
+         * given a solution. */
+
+
+
+        public PositionRotation ForwardKinematics(float[] Solution)
+        {
+            Vector3 prevPoint = Joints[0].transform.position;
+            //Quaternion rotation = Quaternion.identity;
+
+            // Takes object initial rotation into account
+            Quaternion rotation = transform.rotation;
+            for (int i = 1; i < Joints.Length; i++)
+            {
+                // Rotates around a new axis
+                rotation *= Quaternion.AngleAxis(Solution[i - 1], Joints[i - 1].Axis);
+                Vector3 nextPoint = prevPoint + rotation * Joints[i].StartOffset;
+
+                if (DebugDraw)
+                    Debug.DrawLine(prevPoint, nextPoint, Color.blue);
+
+                prevPoint = nextPoint;
+            }
+
+            // The end of the effector
+            return new PositionRotation(prevPoint, rotation);
+        }
+    }
+}
