4df56edd85325c5b161734a806505650a8772500
StormValen <valentin.g.l@gmail.com>
2018-01-02 00:32:36 +0100

    Pintar trayectoria pelota

---
+++ PracticaMecanicaFutbol/Assets/Scripts/Trayectoria.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trayectoria : MonoBehaviour {
+
+    public Transform tPelota;
+    public Material newMat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject path = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        path.transform.localScale = new Vector3(0.03F, 0.03F, 0.03F);
+        path.transform.position = tPelota.position;
+        path.GetComponent<Renderer>().material = newMat;
+	}
+}
